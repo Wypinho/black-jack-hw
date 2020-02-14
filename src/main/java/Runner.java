672e5ac9b1d2a3268c859eb661c9dbb20e19f118
Andrew Wyper <andy_wyper@yahoo.co.uk>
2020-02-14 15:33:55 +0000

    game runs as glorified hi lo

--- src/main/java/Runner.java
+++ src/main/java/Runner.java
@@ -9,41 +9,59 @@ public class Runner {
         Deck deck = new Deck();
         IScorer scorer = new PlayerScorerByRankOrder();
         Game game = new Game(deck, scorer);
-        System.out.println("Welcome to Draw Hi/Lo!");
-        System.out.println("How many players would you like to play?");
+        System.out.println("Welcome to BlackJack!");
+//        System.out.println("How many players would you like to play?");
 
-        String input = scanner.next();
-        int players = parseInt(input);
+//        String input = scanner.next();
+//        int players = parseInt(input);
 
-        for(int i = 0; i < players; i++){
-            String prompt = String.format("Player %s, enter your name: ", (i + 1));
-            System.out.println(prompt);
-            String playerName = scanner.next();
-            Player player = new Player(playerName);
-            game.addPlayer(player);
+//        for(int i = 0; i < players; i++){
+//            String prompt = String.format("Player %s, enter your name: ", (i + 1));
+//            System.out.println(prompt);
+//            String playerName = scanner.next();
+//            Player player = new Player(playerName);
+//            game.addPlayer(player);
+//        }
+
+        String prompt = String.format("Enter your name: ");
+        System.out.println(prompt);
+        String playerName = scanner.next();
+        Player player1 = new Player(playerName);
+        game.addPlayer(player1);
+
+        Player dealer = new Player("Del");
+        game.addPlayer(dealer);
+//        System.out.println("How many cards are we playing with?");
+//        int noOfCards = parseInt(scanner.next());
+
+        game.start(2);
+//
+//        System.out.println("Dealer has:");
+//        System.out.println(dealer.showCard(0));
+
+        String output = String.format("%s has:", dealer.getName());
+        System.out.println(output);
+        for(int i = 0; i < dealer.cardCount(); i ++){
+            System.out.println(dealer.showCard(i));
         }
+        System.out.println(String.format("Hand total: %s", scorer.getScore(dealer)));
 
-        System.out.println("How many cards are we playing with?");
-        int noOfCards = parseInt(scanner.next());
-
-        game.start(noOfCards);
-
-        for(Player player: game.getPlayers()){
-            String output = String.format("%s has:", player.getName());
-            System.out.println(output);
-            for(int i = 0; i < player.cardCount(); i ++){
-                System.out.println(player.showCard(i));
+//        for(Player player: game.getPlayers()){
+            String output2 = String.format("%s has:", player1.getName());
+            System.out.println(output2);
+            for(int i = 0; i < player1.cardCount(); i ++){
+                System.out.println(player1.showCard(i));
             }
-            System.out.println(String.format("Hand total: %s", scorer.getScore(player)));
-        }
+            System.out.println(String.format("Hand total: %s", scorer.getScore(player1)));
+//        }
 
         if(game.checkDraw()){
             System.out.println("It's a draw!");
         } else {
             Player winner = game.checkWinner();
             String winnerName = winner.getName();
-            String output = String.format("%s wins!", winnerName);
-            System.out.println(output);
+            String output3 = String.format("%s wins!", winnerName);
+            System.out.println(output3);
         }
 
     }
