55761f7090ef337a00225c2690ef45c85cd36f61
Andrew Wyper <andy_wyper@yahoo.co.uk>
2020-02-14 13:48:00 +0000

    scorer amended for aces high or low

--- src/main/java/PlayerScorerByRankOrder.java
+++ src/main/java/PlayerScorerByRankOrder.java
@@ -3,7 +3,17 @@ public class PlayerScorerByRankOrder implements IScorer {
     public int getScore(Player player){
         int score = 0;
         for(Card card : player.getHand()){
-            score += card.getRank().ordinal() + 1;
+            if (card.getRank().ordinal() == 0){
+                if (score + 11 > 21) {
+                    score += 1;
+                } else {
+                    score += 11;
+                }
+            } else if (card.isFaceCard()) {
+                score += 10;
+            } else {
+                score += card.getRank().ordinal() + 1;
+            }
         }
         return score;
     }
--- src/test/java/PlayerScorerByRankOrderTest.java
+++ src/test/java/PlayerScorerByRankOrderTest.java
@@ -6,25 +6,39 @@ public class PlayerScorerByRankOrderTest {
 
 
     @Test
-    public void testScoreForKings(){
+    public void testScoreForFaceCards(){
+        Card jack = new Card(Suit.CLUBS, Rank.JACK);
+        Card queen = new Card(Suit.HEARTS, Rank.QUEEN);
         Card king = new Card(Suit.DIAMONDS, Rank.KING);
-        Player player = new Player("Alex");
-        player.takeCard(king);
+        Player player = new Player("Andrew");
+        player.takeCard(jack);
+        player.takeCard(queen);
         player.takeCard(king);
         IScorer scorer = new PlayerScorerByRankOrder();
         int actualScore = scorer.getScore(player);
-        assertEquals(26, actualScore);
+        assertEquals(30, actualScore);
     }
 
     @Test
-    public void testScoreForAces(){
+    public void testScoreForAcesHigh(){
         Card ace = new Card(Suit.DIAMONDS, Rank.ACE);
-        Player player = new Player("Alex");
-        player.takeCard(ace);
+        Player player = new Player("Andrew");
         player.takeCard(ace);
         IScorer scorer = new PlayerScorerByRankOrder();
         int actualScore = scorer.getScore(player);
-        assertEquals(2, actualScore);
+        assertEquals(11, actualScore);
+    }
+
+    @Test
+    public void testScoreForAcesLowAfterFirst(){
+        Card ace = new Card(Suit.SPADES, Rank.ACE);
+        Card ace2 = new Card(Suit.HEARTS, Rank.ACE);
+        Player player = new Player("Andrew");
+        player.takeCard(ace);
+        player.takeCard(ace2);
+        IScorer scorer = new PlayerScorerByRankOrder();
+        int actualScore = scorer.getScore(player);
+        assertEquals(12, actualScore);
     }
 
 
